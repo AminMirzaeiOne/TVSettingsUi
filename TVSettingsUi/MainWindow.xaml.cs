57aa30f601b878e1e51f84281605160655def60f
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-14 15:22:34 +0330

    - Create Grid_MouseLeftButtonDown Method In MainWindow.CS

--- TVSettingsUi/MainWindow.xaml.cs
+++ TVSettingsUi/MainWindow.xaml.cs
@@ -78,5 +78,26 @@ namespace TVSettingsUi
             }
         }
 
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximize)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1280;
+                    this.Height = 780;
+
+                    IsMaximize = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximize = true;
+                }
+            }
+        }
+
     }
 }
